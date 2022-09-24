e930cd4afd64b849d35925bd5a704c68e5647aa4
EdmundoTriguero <edmundotriguero@gmail.com>
2022-09-24 01:11:40 -0400

    login implementado

--- bases/urls.py
+++ bases/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from bases.views import inicio, HomesinPrivilegios
+from bases.views import inicio, HomesinPrivilegios,registro ,profile
 
 # importar para las vistas de login
 from django.urls import path, reverse_lazy
@@ -17,9 +17,12 @@ urlpatterns = [
 
 
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'), name='login'),
+    # path('login/', Login.as_view(template_name='bases/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='bases/login.html'), name='logout'),
-
+    path('registration/',registro,name='registration'),
     path('sin_privilegios/', HomesinPrivilegios.as_view(), name='sin_privilegios'),
+
+     path('profile/',profile,name='profile'),
 
 #     path('reset/lasante', auth_views.PasswordResetView.as_view(template_name='bases/password_reset_form.html', email_template_name='bases/password_reset_email.html', success_url=reverse_lazy('bases:password_reset_done')),
 #          name='reset', ),
--- bases/views.py
+++ bases/views.py
@@ -1,12 +1,22 @@
 from multiprocessing import context
+from re import template
+from tokenize import group
 from urllib import request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 from django.http import HttpResponseRedirect
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin ,  PermissionRequiredMixin
 # Create your views here.
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+
+from django.contrib.auth.models import Group
+
+
+from django.contrib.auth.views import LoginView
+
 
 
 def inicio(request):
@@ -33,3 +43,57 @@ class SinPrivilegios(LoginRequiredMixin, PermissionRequiredMixin):
 
 class HomesinPrivilegios(generic.TemplateView):
     template_name="bases/error_400.html"
+
+
+
+def registro(request):
+    template = 'bases/registration.html'
+    obj = { 'form':CustomUserCreationForm }
+
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            
+            user_new = formulario.save()
+            group = Group.objects.get(name='cliente')
+            user_new.groups.add(group)
+            user = authenticate(username=formulario.cleaned_data['username'], password=formulario.cleaned_data['password1'])
+            login(request, user)
+            print('Redirigiendo')
+            return redirect('web:index')
+        
+
+    return render(request, template,obj)
+
+
+
+# class Login(LoginView):
+#     authentication_form = CustomAuthenticationForm
+
+#     form_class = CustomAuthenticationForm
+
+#     template_name = 'login.html'
+
+#     def form_valid(self, form):
+
+#         #remember_me = form.cleaned_data['remember_me']
+
+#         login(self.request, form.get_user())
+
+#         user = form.get_user()
+
+#         group = user.groups
+
+#         print(group)    
+#         # if remember_me:
+
+#         #     self.request.session.set_expiry(1209600)
+
+#         return super(LoginView, self).form_valid(form)
+
+
+def profile(request):
+    template_name = 'bases/profile.html'
+    contexto = {}
+
+    return render(request, template_name, contexto)
